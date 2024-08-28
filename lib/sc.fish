b444c357d35e1825ac3b20ff76f9411f55e3e033
rnjn <runjan@gmail.com>
2024-08-28 23:22:05 +0530

    slightly uglier but more practical way to check if repo is dirty before pulling

--- lib/sc.fish
+++ lib/sc.fish
@@ -5,9 +5,10 @@ function pull-all
         if test -d $dir && test -d "$dir/.git"
             echo "Pulling latest changes for --- $dir ---"
             cd $dir
-            if git diff-index --quiet HEAD --;
+            if git ls-files --others --exclude-standard;
                 git pull --rebase
             else
+                echo (git ls-files --others --exclude-standard)
                 echo (set_color red) "Repo in $dir is dirty and needs a commit first" (set_color normal)
             end
             cd ..
